d7f50930903513fbad289b6cd0470951096210cf
phoenixgoldz <thicks@student.neumont.edu>
2025-01-30 12:17:02 -0700

    update main scene and options menu

--- GnomeVsKnights/Assets/Scripts/OptionsMenu.cs
+++ GnomeVsKnights/Assets/Scripts/OptionsMenu.cs
@@ -1,42 +1,71 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.Audio;
 using System.Collections;
 
 public class OptionsMenu : MonoBehaviour
 {
     public GameObject optionsPanel;
-    public Slider volumeSlider;
-    public TMP_Text volumeText;
-    public Toggle fullscreenToggle;
+    public Slider musicVolumeSlider;
+    public Slider sfxVolumeSlider;
+    public TMP_Text musicVolumeText;
+    public TMP_Text sfxVolumeText;
+    public Toggle subtitlesToggle;
     public Button applyButton;
+    public Button restoreDefaultsButton;
+    public Button closeButton;
     public TMP_Text saveMessage;
+    public AudioSource buttonClickAudioSource;
+    public AudioClip buttonClickSound;
+    public Image savingImage;
+    public AudioMixer audioMixer;
+    public RectTransform savingImageTransform;
+    public Dropdown musicSelectionDropdown;
+    public AudioSource musicSource;
+    public AudioClip[] musicTracks;
 
-    private float tempVolume;
-    private bool tempFullscreen;
+    private float tempMusicVolume;
+    private float tempSfxVolume;
+    private bool tempSubtitles;
 
     private void Start()
     {
         optionsPanel.SetActive(false);
         saveMessage.gameObject.SetActive(false);
+        savingImage.gameObject.SetActive(false);
 
         // Load saved settings
-        tempVolume = PlayerPrefs.GetFloat("Volume", 0.5f);
-        tempFullscreen = PlayerPrefs.GetInt("Fullscreen", 1) == 1;
+        tempMusicVolume = PlayerPrefs.GetFloat("MusicVolume", 0.5f);
+        tempSfxVolume = PlayerPrefs.GetFloat("SfxVolume", 0.5f);
+        tempSubtitles = PlayerPrefs.GetInt("Subtitles", 1) == 1;
 
-        volumeSlider.value = tempVolume;
-        fullscreenToggle.isOn = tempFullscreen;
-        UpdateVolumeText(tempVolume);
+        // Apply settings to UI
+        musicVolumeSlider.value = tempMusicVolume;
+        sfxVolumeSlider.value = tempSfxVolume;
+        subtitlesToggle.isOn = tempSubtitles;
 
+        // Populate music selection dropdown
+        musicSelectionDropdown.ClearOptions();
+        foreach (AudioClip track in musicTracks)
+        {
+            musicSelectionDropdown.options.Add(new Dropdown.OptionData(track.name));
+        }
+        musicSelectionDropdown.onValueChanged.AddListener(ChangeMusicTrack);
+        
         // Add Listeners
-        volumeSlider.onValueChanged.AddListener(UpdateVolumeText);
-        fullscreenToggle.onValueChanged.AddListener(value => tempFullscreen = value);
-        applyButton.onClick.AddListener(ApplySettings);
+        musicVolumeSlider.onValueChanged.AddListener(UpdateMusicVolume);
+        sfxVolumeSlider.onValueChanged.AddListener(UpdateSfxVolume);
+        subtitlesToggle.onValueChanged.AddListener(value => tempSubtitles = value);
+        applyButton.onClick.AddListener(() => { PlayButtonSound(); ApplySettings(); });
+        restoreDefaultsButton.onClick.AddListener(() => { PlayButtonSound(); RestoreDefaults(); });
+        closeButton.onClick.AddListener(() => { PlayButtonSound(); CloseOptions(); });
     }
 
     public void OpenOptions()
     {
         optionsPanel.SetActive(true);
+        PlayButtonSound();
     }
 
     public void CloseOptions()
@@ -46,28 +75,69 @@ public class OptionsMenu : MonoBehaviour
 
     public void ApplySettings()
     {
-        PlayerPrefs.SetFloat("Volume", tempVolume);
-        PlayerPrefs.SetInt("Fullscreen", tempFullscreen ? 1 : 0);
+        PlayerPrefs.SetFloat("MusicVolume", tempMusicVolume);
+        PlayerPrefs.SetFloat("SfxVolume", tempSfxVolume);
+        PlayerPrefs.SetInt("Subtitles", tempSubtitles ? 1 : 0);
         PlayerPrefs.Save();
 
-        AudioListener.volume = tempVolume;
-        Screen.fullScreen = tempFullscreen;
-
-        Debug.Log("Settings Applied: Volume = " + (tempVolume * 100) + "% Fullscreen = " + tempFullscreen);
+        //Debug.Log("Settings Applied: Music Volume = " + (tempMusicVolume * 100) + "%", "SFX Volume = " + (tempSfxVolume * 100) + "%", "Subtitles = " + tempSubtitles);
         StartCoroutine(ShowSaveMessage());
     }
 
-    private void UpdateVolumeText(float volume)
+    public void RestoreDefaults()
     {
-        volumeText.text = Mathf.RoundToInt(volume * 100) + "%";
-        tempVolume = volume;
+        musicVolumeSlider.value = 0.5f;
+        sfxVolumeSlider.value = 0.5f;
+        subtitlesToggle.isOn = true;
+        musicSelectionDropdown.value = 0;
+    }
+
+    private void UpdateMusicVolume(float volume)
+    {
+        tempMusicVolume = volume;
+        musicVolumeText.text = Mathf.RoundToInt(volume * 100) + "%";
+        audioMixer.SetFloat("MusicVolume", Mathf.Log10(Mathf.Max(volume, 0.0001f)) * 20);
+    }
+
+    private void UpdateSfxVolume(float volume)
+    {
+        tempSfxVolume = volume;
+        sfxVolumeText.text = Mathf.RoundToInt(volume * 100) + "%";
+        audioMixer.SetFloat("SfxVolume", Mathf.Log10(Mathf.Max(volume, 0.0001f)) * 20);
     }
 
     private IEnumerator ShowSaveMessage()
     {
+        savingImage.gameObject.SetActive(true);
         saveMessage.gameObject.SetActive(true);
-        saveMessage.text = "Settings Saved!";
-        yield return new WaitForSeconds(1.5f);
+        saveMessage.text = "Saving...";
+        
+        float elapsedTime = 0f;
+        while (elapsedTime < 2.5f)
+        {
+            savingImageTransform.Rotate(0f, 0f, 100f * Time.deltaTime);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        
         saveMessage.gameObject.SetActive(false);
+        savingImage.gameObject.SetActive(false);
+    }
+
+    private void PlayButtonSound()
+    {
+        if (buttonClickAudioSource != null && buttonClickSound != null)
+        {
+            buttonClickAudioSource.PlayOneShot(buttonClickSound);
+        }
+    }
+
+    private void ChangeMusicTrack(int index)
+    {
+        if (musicTracks.Length > index && musicTracks[index] != null)
+        {
+            musicSource.clip = musicTracks[index];
+            musicSource.Play();
+        }
     }
 }
