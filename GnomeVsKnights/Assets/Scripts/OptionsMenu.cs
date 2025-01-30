62ccce32243476b9d728abd58ec388b2add19011
phoenixgoldz <thicks@student.neumont.edu>
2025-01-30 11:08:30 -0700

    update to screen size in game viewer to match mobile view 16:9

--- GnomeVsKnights/Assets/Scripts/OptionsMenu.cs
+++ GnomeVsKnights/Assets/Scripts/OptionsMenu.cs
@@ -1,81 +1,37 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using System.Collections;
-using System.Collections.Generic;
 
 public class OptionsMenu : MonoBehaviour
 {
     public GameObject optionsPanel;
     public Slider volumeSlider;
     public TMP_Text volumeText;
-    public Dropdown resolutionDropdown;
     public Toggle fullscreenToggle;
     public Button applyButton;
-    public TMP_Text saveMessage; // Text to display when settings are applied
-    public Image savingImage; // The rotating saving image
+    public TMP_Text saveMessage;
 
     private float tempVolume;
     private bool tempFullscreen;
-    private Resolution[] resolutions;
-    private List<string> resolutionOptions = new List<string>();
-    private int currentResolutionIndex;
-    private bool isSaving = false;
 
     private void Start()
     {
         optionsPanel.SetActive(false);
-        saveMessage.gameObject.SetActive(false); // Hide message initially
-        savingImage.enabled = false; // Hide saving image initially
+        saveMessage.gameObject.SetActive(false);
 
-        // Load saved settings or set default values
-        float savedVolume = PlayerPrefs.HasKey("Volume") ? PlayerPrefs.GetFloat("Volume") : 0.5f;
-        bool savedFullscreen = PlayerPrefs.GetInt("Fullscreen", 1) == 1;
-        int savedResolutionIndex = PlayerPrefs.GetInt("ResolutionIndex", 0);
+        // Load saved settings
+        tempVolume = PlayerPrefs.GetFloat("Volume", 0.5f);
+        tempFullscreen = PlayerPrefs.GetInt("Fullscreen", 1) == 1;
 
-        // Apply volume settings
-        volumeSlider.value = savedVolume;
-        fullscreenToggle.isOn = savedFullscreen;
-        UpdateVolumeText(savedVolume);
-
-        // Store temporary settings
-        tempVolume = savedVolume;
-        tempFullscreen = savedFullscreen;
-
-        // Populate resolution dropdown
-        resolutions = Screen.resolutions;
-        resolutionDropdown.ClearOptions();
-
-        for (int i = 0; i < resolutions.Length; i++)
-        {
-            string resolutionString = resolutions[i].width + " x " + resolutions[i].height;
-            resolutionOptions.Add(resolutionString);
-
-            if (resolutions[i].width == Screen.currentResolution.width &&
-                resolutions[i].height == Screen.currentResolution.height)
-            {
-                currentResolutionIndex = i;
-            }
-        }
-
-        resolutionDropdown.AddOptions(resolutionOptions);
-        resolutionDropdown.value = PlayerPrefs.HasKey("ResolutionIndex") ? savedResolutionIndex : currentResolutionIndex;
-        resolutionDropdown.RefreshShownValue();
+        volumeSlider.value = tempVolume;
+        fullscreenToggle.isOn = tempFullscreen;
+        UpdateVolumeText(tempVolume);
 
         // Add Listeners
         volumeSlider.onValueChanged.AddListener(UpdateVolumeText);
         fullscreenToggle.onValueChanged.AddListener(value => tempFullscreen = value);
-        resolutionDropdown.onValueChanged.AddListener(SetResolution);
         applyButton.onClick.AddListener(ApplySettings);
-    }
-
-    private void Update()
-    {
-        // Rotate the saving image while saving
-        if (isSaving && savingImage.enabled)
-        {
-            savingImage.transform.Rotate(0, 0, 200 * Time.deltaTime);
-        }
     }
 
     public void OpenOptions()
@@ -90,42 +46,15 @@ public class OptionsMenu : MonoBehaviour
 
     public void ApplySettings()
     {
-        // Save settings
         PlayerPrefs.SetFloat("Volume", tempVolume);
         PlayerPrefs.SetInt("Fullscreen", tempFullscreen ? 1 : 0);
-        PlayerPrefs.SetInt("ResolutionIndex", resolutionDropdown.value);
         PlayerPrefs.Save();
 
-        // Apply settings
         AudioListener.volume = tempVolume;
         Screen.fullScreen = tempFullscreen;
-        Screen.SetResolution(resolutions[resolutionDropdown.value].width, resolutions[resolutionDropdown.value].height, Screen.fullScreen);
 
-        Debug.Log("Settings Applied: Volume = " + (tempVolume * 100) + "%, Fullscreen = " + tempFullscreen +
-                  ", Resolution = " + resolutionOptions[resolutionDropdown.value]);
-
-        // Show and rotate the saving image
-        StartCoroutine(ShowSavingImage());
-
-        // Show save confirmation message
+        Debug.Log("Settings Applied: Volume = " + (tempVolume * 100) + "% Fullscreen = " + tempFullscreen);
         StartCoroutine(ShowSaveMessage());
-    }
-
-    private IEnumerator ShowSavingImage()
-    {
-        isSaving = true;
-        savingImage.enabled = true;
-        yield return new WaitForSeconds(2.5f);
-        isSaving = false;
-        savingImage.enabled = false;
-    }
-
-    private IEnumerator ShowSaveMessage()
-    {
-        saveMessage.gameObject.SetActive(true);
-        saveMessage.text = "Saving...";
-        yield return new WaitForSeconds(2.5f);
-        saveMessage.gameObject.SetActive(false);
     }
 
     private void UpdateVolumeText(float volume)
@@ -134,8 +63,11 @@ public class OptionsMenu : MonoBehaviour
         tempVolume = volume;
     }
 
-    private void SetResolution(int index)
+    private IEnumerator ShowSaveMessage()
     {
-        currentResolutionIndex = index;
+        saveMessage.gameObject.SetActive(true);
+        saveMessage.text = "Settings Saved!";
+        yield return new WaitForSeconds(1.5f);
+        saveMessage.gameObject.SetActive(false);
     }
 }
