761777cd5bf35c6b10946b44ac4aeee4462f685f
phoenixgoldz <thicks@student.neumont.edu>
2025-02-05 12:41:33 -0700

    update to main

--- GnomeVsKnights/Assets/Scripts/VolumeSettings.cs
+++ GnomeVsKnights/Assets/Scripts/VolumeSettings.cs
@@ -1,18 +1,18 @@
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
-
+using TMPro;
 
 public class VolumeSliders : MonoBehaviour
 {
-
     [SerializeField] private AudioMixer myMixer;
     [SerializeField] private Slider musicSlider;
     [SerializeField] private Slider SFXSlider;
+    [SerializeField] private TMP_Text musicVolumeText; // TextMeshPro Text for music volume
+    [SerializeField] private TMP_Text SFXVolumeText;   // TextMeshPro Text for SFX volume
 
-    public void Start()
+    private void Start()
     {
-
         if (PlayerPrefs.HasKey("musicVolume"))
         {
             LoadVolume();
@@ -21,21 +21,31 @@ public class VolumeSliders : MonoBehaviour
         {
             SetMusicVolume();
             SetSFXVolume();
-
         }
 
+        // Add listener to update text when slider value changes
+        musicSlider.onValueChanged.AddListener(delegate { SetMusicVolume(); });
+        SFXSlider.onValueChanged.AddListener(delegate { SetSFXVolume(); });
     }
+
     public void SetMusicVolume()
     {
         float volume = musicSlider.value;
         myMixer.SetFloat("music", Mathf.Log10(volume) * 20);
         PlayerPrefs.SetFloat("musicVolume", volume);
+
+        // Update the UI text to reflect the new volume percentage
+        musicVolumeText.text = Mathf.RoundToInt(volume * 100) + "%";
     }
-     public void SetSFXVolume()
+
+    public void SetSFXVolume()
     {
         float volume = SFXSlider.value;
         myMixer.SetFloat("SFX", Mathf.Log10(volume) * 20);
         PlayerPrefs.SetFloat("SFXVolume", volume);
+
+        // Update the UI text to reflect the new volume percentage
+        SFXVolumeText.text = Mathf.RoundToInt(volume * 100) + "%";
     }
 
     private void LoadVolume()
@@ -46,5 +56,4 @@ public class VolumeSliders : MonoBehaviour
         SetMusicVolume();
         SetSFXVolume();
     }
-
 }
