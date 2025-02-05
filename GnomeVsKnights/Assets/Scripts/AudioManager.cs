ce57cc38bbfc8b5c49fe41d6b548a498542a98bf
phoenixgoldz <thicks@student.neumont.edu>
2025-02-05 11:06:19 -0700

    update to audio files and options menu
    RPG Essentials Sound Effects - FREE!
    - updated main menu to be full screen
    - updated audio files and added sfx

---
+++ GnomeVsKnights/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using Unity.UI;
+using UnityEngine.UIElements;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header("------Audio Source-----")]
+    [SerializeField] private AudioSource MusicSource;
+    [SerializeField] private Slider musicSlider; 
+    [SerializeField] private AudioSource SFXSource;
+    [SerializeField] private Slider soundSlider;
+
+    [Header("------Audio Clips-----")]
+    public AudioClip backgroundMusic;
+    public AudioClip death;
+    public AudioClip attack;
+    public AudioClip spawnGnome;
+    public AudioClip spawnEnemy;
+
+
+    private void Start()
+    {
+        MusicSource.clip = backgroundMusic;
+        MusicSource.Play();
+    }
+}
